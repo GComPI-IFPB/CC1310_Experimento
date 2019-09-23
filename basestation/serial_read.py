8b2da07a008fc49142da8942bbb9412e5640ace8
Allan Bispo <allanbispoxd@gmail.com>
2019-09-23 17:20:37 -0300

    Atualização na formatação de saída da Serial
    Algumas pequenas mudanças na formatação da saída do dado no terminal serial.

--- basestation/serial_read.py
+++ basestation/serial_read.py
@@ -1,5 +1,6 @@
 import serial
 
+
 def convert(byte):
     if byte > 127:
         return (256-byte)*(-1)
@@ -9,7 +10,8 @@ def convert(byte):
 with serial.Serial("COM9", 115200) as sr:
     while True:
         string = sr.read(32)
-        print("Dado:", string[0:30].hex(), end=" ")
-        print("RSSI:", convert(int.from_bytes(string[31:32], byteorder='big')))
-
+        print("| ID:", string[0], "|", end=" ")
+        print("SeqNumber:", int.from_bytes(string[1:3], byteorder="big"), "|", end=" ")
+        print("Dado:", string[3:30].hex(), "|", end=" ")
+        print("RSSI:", convert(int.from_bytes(string[31:32], byteorder="big")), "|")
 
