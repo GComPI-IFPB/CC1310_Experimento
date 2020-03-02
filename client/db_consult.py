46f508ce7a624a68acc8cdcfe1f5fd132cade484
allanbn <allanbispoxd@gmail.com>
2020-03-02 11:09:09 -0300

    db_consult modificado

--- client/db_consult.py
+++ client/db_consult.py
@@ -1,11 +1,24 @@
 from influxdb import InfluxDBClient
 
+
+def createList():
+    l = client.query('SHOW MEASUREMENTS')
+    l = list(l.items()[0][1])
+    new_list = []
+    for i in l:
+        new_list.append(i['name'])
+
+    return new_list
+
+
 client = InfluxDBClient(host='150.165.254.119', username='client', password='', database='CC1310_Packets', port=8086)
-line = input()
-rs = client.query('{}'.format(line))
-print(line)
-for i in rs.get_points(measurement='packet'):
+# line = input()
+rs = createList()
+print("Lista de tabelas:")
+for i in range(len(rs)):
+    print(i, rs[i])
+
+query = input("\nDigite a query: ")
+rs = client.query(query)
+for i in rs.get_points():
     print(i)
-
-
-# SELECT * FROM packet WHERE (sourceID='1') AND (time > now() - 1h) limit 1
